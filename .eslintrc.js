03132f54d19134c3cf8622801400305352cbe9e7
lttb <kenzhaev.artur@gmail.com>
2018-06-19 16:48:33 +0300

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:flowtype/recommended',
+  ],
+
+  rules: {
+    'no-unused-expressions': 'off',
+    'no-multi-assign': 'off',
+    semi: ['error', 'never'],
+    'object-curly-spacing': 'off',
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+    'no-underscore-dangle': 'off',
+
+    'import/prefer-default-export': 'off',
+
+    'flowtype/no-unused-expressions': 'error',
+    'flowtype/generic-spacing': 'off',
+  },
+
+  plugins: ['flowtype'],
+}
